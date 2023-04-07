39f3321d54c9cbe2d60e242ce060bc153a489e57
Khurram Raheel <khurramraheel@yahoo.com>
2023-04-07 10:18:47 +0500

    sadsad

--- app.js
+++ app.js
@@ -182,9 +182,9 @@ meriApp.delete('/user-delete', function(req, res){
 
 });
 
-meriApp.post('/create-user', upload.array('pic',20), function(req, res){
+meriApp.post('/create-user', function(req, res){
 
-    req.body.pic = req.files[0].originalname;
+    // req.body.pic = req.files[0].originalname;
 
     users.push(req.body);
     console.log(req.body)
